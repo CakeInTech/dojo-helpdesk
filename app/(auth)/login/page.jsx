981e5336001c6f0b91e5404a7b3eaee664effc09
Mohamed Abdulhakim <abdulhakimm280@gmail.com>
2023-10-26 15:40:58 +0200

    Create auth folder and move the rest to the dashboard

---
+++ app/(auth)/login/page.jsx
@@ -0,0 +1,9 @@
+function Login() {
+    return (
+        <main>
+            <h2 className="text-center">Log In</h2>
+        </main>
+    )
+}
+
+export default Login
---
+++ app/(auth)/signup/page.jsx
@@ -0,0 +1,9 @@
+function SignUp() {
+    return (
+        <main>
+            <h2 className="text-center">Sign Up</h2>
+        </main>
+    )
+}
+
+export default SignUp
